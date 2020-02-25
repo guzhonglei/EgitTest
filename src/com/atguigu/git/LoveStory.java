9736bbc902d1d3176d5e238be99e25f1bd770b93
zhangsan <zhangsan@qq.com>
2020-02-25 23:17:55 +0800

    Sorry,we can make friend!!!!!

--- src/com/atguigu/git/LoveStory.java
+++ src/com/atguigu/git/LoveStory.java
@@ -5,6 +5,7 @@ public class LoveStory {
 	public static void main(String[] args) {
 		System.out.println("我喜欢你！！！！———zhangsan");
 		System.out.println("I don't like you!!!!");
+		System.out.println("Sorry,we can make friend!!!!!");
 	}
 	
 }
